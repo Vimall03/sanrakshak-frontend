9f0f9f4d039bd1c92b539f2e21243af3e78e4e5f
Vimal M <109758195+Vimall03@users.noreply.github.com>
2024-04-12 23:39:16 +0530

    my changes

--- pages/police/index.js
+++ pages/police/index.js
@@ -1,35 +1,31 @@
-import React from 'react'
-import { useRouter } from 'next/router'
-import axios from 'axios'
-import Cookies from 'js-cookie'
+import React, { useState } from 'react'
+
 import PoliceLayout from '../layout/PoliceLayout'
 import Panel from '@/components/common/Leftpanel.js/Panel'
+import CaseView from '@/components/modals/CaseView'
+
 
 const PoliceDashboard = () => {
-  const router = useRouter()
-  const handleLogout = async () => {
-    console.log('logout clicked')
-    try {
-      const reponse = await axios.get('http://localhost:4000/api/auth/logout', {
-        withCredentials: true
-      });
-      console.log(reponse)
-      if (reponse.data.success == true) {
-        console.log('response sucess');
-        Cookies.remove('accessToken')
-        router.push('/login')
-      }
-
-    } catch (error) {
-      console.log(error)
-    }
+  const [visible, setVisible] = useState(false)
+  const onClose = () => {
+    setVisible(false)
+  }
+  const onCall = () => {
+    setVisible(true)
+  }
+  const data = {
+    
   }
   return (
     <PoliceLayout>
       <div className=" bg-[#080F25] w-[100%] h-[100vh] flex flex-row relative flex-shrink ">
         <Panel />
-        <div className=" w-[100%] h-[81vh] flex  flex-col items-center px-[2.5rem] pt-[1rem] text-[#AEB9E1] text-[2rem] ">
-          <h1>hello from police dashboard</h1>
+        <div className=" w-[100%] h-[81vh] flex  flex-col  px-[2.5rem] pt-[1rem] text-[#AEB9E1] text-[2rem] ">
+          <h1 className='font-semibold'>Dashboard</h1>
+          <div className='items-center'>
+            <button className="p-2 focus:outline-none text-[#D3DCFF] text-base font-medium hover:text-[#9badff] rounded-md border border-[#D3DCFF]" onClick={onCall}>View Case</button>
+            <CaseView visible={visible} data={data} onClose={onClose} />
+          </div>
         </div>
       </div>
     </PoliceLayout>
